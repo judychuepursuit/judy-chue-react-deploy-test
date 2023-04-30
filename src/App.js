ed59443773c1c591c0686018a62d34e1465ea731
Judy <judychue@pursuit.org>
2023-04-30 10:40:45 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import pizza from './pizza.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={pizza} className="App-logo" alt="logo" />
+        <p>
+        </p>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
