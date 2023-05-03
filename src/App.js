fe76029a566aec338484c9b69cec92c1083700ab
Judy <judychue@pursuit.org>
2023-05-03 16:43:09 -0400

    Judy's Pizza Parlor

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Judy's Pizza Parlor
         </a>
       </header>
     </div>
